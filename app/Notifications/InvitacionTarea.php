7a659e93073b9d573dd0ae626408dbb3407b2a83
Paloma-Beltran <paloma.beltran4396@alumnos.udg.mx>
2025-04-27 19:31:53 -0600

    Se implemento la parte del envío de correos cuando se invita a un usuario a una tarea

---
+++ app/Notifications/InvitacionTarea.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+use App\Models\Tarea;
+
+class InvitacionTarea extends Notification
+{
+    use Queueable;
+
+    public $tarea;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Tarea $tarea)
+    {
+        $this->tarea = $tarea;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Invitación a una tarea')
+            ->greeting('¡Hola ' . $notifiable->name . '!')
+            ->line('Has sido invitado a colaborar en la tarea: "' . $this->tarea->nombre . '"')
+            ->action('Ver tarea', url('/tareas/' . $this->tarea->id))
+            ->line('Gracias por usar nuestra aplicación.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
